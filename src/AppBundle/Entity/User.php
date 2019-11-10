0be00cc32d432470334b20bacc10bb4b470c6c2b
rubenmorcillo <ravent953@gmail.com>
2019-11-10 11:43:01 +0100

    Probando doctrine

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     *
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $nickname;
+    
+}
