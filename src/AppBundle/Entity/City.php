71443c227ee53bedcbb1522404f0415ac015ceef
rubenmorcillo <ravent953@gmail.com>
2019-11-14 19:31:18 +0100

    Creada entidad ColectivoLocal y su relación con City

--- src/AppBundle/Entity/City.php
+++ src/AppBundle/Entity/City.php
@@ -3,6 +3,7 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -12,6 +13,13 @@ use Doctrine\ORM\Mapping as ORM;
 class City
 {
 
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
 
     /**
      * @ORM\Column(type="string")
@@ -20,6 +28,25 @@ class City
     private $personas;
 
 
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\ColectivoLocal",mappedBy="ciudad")
+     * @var ColectivoLocal[]
+     */
+    private $colectivos;
+
+
+    public function __construct()
+    {
+        $this->personas = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
     /**
      * @param mixed $personas
@@ -39,10 +66,22 @@ class City
         return $this->personas;
     }
 
-
-    public function __construct(array $personas)
+    /**
+     * @return ColectivoLocal[]
+     */
+    public function getColectivos()
     {
-        $this->personas = $personas;
+        return $this->colectivos;
+    }
+
+    /**
+     * @param ColectivoLocal[] $colectivos
+     * @return City
+     */
+    public function setColectivos($colectivos)
+    {
+        $this->colectivos = $colectivos;
+        return $this;
     }
 
 }
