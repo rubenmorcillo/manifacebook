f3c34bc8ac6c81cd2cdde713f78db38cb4643981
Javier <rotronox@hotmail.com>
2019-12-11 21:40:44 +0100

    añado listado de ciudades

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Repository\CityRepositoy;
 use AppBundle\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,12 +13,13 @@ class UserController extends Controller
     /**
      * @Route("/user", name="listar_user")
      */
-    public function listarAction(UserRepository $userRepository)
+    public function listarAction(UserRepository $userRepository,CityRepositoy $cityRepository)
     {
         $users = $userRepository->findAll();
-
+        $city = $cityRepository->findAll();
         return $this->render('default/index.html.twig', [
-            'usuarios' => $users
+            'usuarios' => $users,
+            'ciudades' => $city
         ]);
     }
 }
